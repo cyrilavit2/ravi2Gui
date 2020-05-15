3f132cc63c819245f2d9e9a16e5ad6679174a200
PC <cyrilavit7@gmail.com>
2020-05-15 20:23:29 +0200

    change 4

--- gui.py
+++ gui.py
@@ -1,12 +1,22 @@
+import json
 import sys
+
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QWidget, QVBoxLayout, QTabWidget, QPushButton, \
-    QInputDialog, QLineEdit
+    QInputDialog, QLineEdit, QTableWidget, QTableWidgetItem, QHBoxLayout
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
 
 class Gui(QMainWindow):
 
-    def __init__(self):
+    def __init__(self,parent=None):
         super().__init__()
         self.initUI()
+
 
     def initUI(self):
         self.statusBar().showMessage('Ready')
@@ -80,12 +90,56 @@ class MyTableWidget(QWidget):
 
         self.tab1.layout.addWidget(openButton)
         self.tab1.setLayout(self.tab1.layout)
-        self.tab1.setStyleSheet("background-image: url(./download.jpg); background-attachment: fixed;")
+        self.tab1.setStyleSheet(("background-image: url(./logositeweb_72ppp.png); background-attachment: fixed;"))
 
+
+        #Tab 2:
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(6)
+        self.tableWidget.setColumnCount(2)
+
+        self.tab2.layout = QHBoxLayout(self)
+        self.tab2.layout.addWidget(self.tableWidget)
+
+
+#(nom, prenom, date de naissance, sexe, taille, poid)
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("nom ?"))
+        self.tableWidget.setItem(1, 0, QTableWidgetItem("Prenom ?"))
+        self.tableWidget.setItem(2, 0, QTableWidgetItem("Date de naissance ?"))
+        self.tableWidget.setItem(3, 0, QTableWidgetItem("Sexe ?"))
+        self.tableWidget.setItem(4, 0, QTableWidgetItem("Taille ?"))
+        self.tableWidget.setItem(5, 0, QTableWidgetItem("Poid ?"))
+
+        saveButton = QPushButton("Sauvegarde ?")
+        saveButton.clicked.connect(self.saveClick)
+        self.tab2.layout.addWidget(saveButton)
+
+        self.tab2.setLayout(self.tab2.layout)
 
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
+
+    def saveClick(self):
+        print("save")
+        dictionnaire = {}
+
+        if self.tableWidget.item(0,1):
+            dictionnaire["nom"] = self.tableWidget.item(0,1).text()
+        if self.tableWidget.item(1, 1):
+            dictionnaire["Prenom"] = self.tableWidget.item(1, 1).text()
+        if self.tableWidget.item(2, 1):
+            dictionnaire["Date"] = self.tableWidget.item(2, 1).text()
+        if self.tableWidget.item(3, 1):
+            dictionnaire["Sexe"] = self.tableWidget.item(3, 1).text()
+        if self.tableWidget.item(4, 1):
+            dictionnaire["Taille"] = self.tableWidget.item(4, 1).text()
+        if self.tableWidget.item(5, 1):
+            dictionnaire["Poids"] = self.tableWidget.item(5, 1).text()
+
+        print(dictionnaire)
+        with open('data.json', 'w') as file:
+            json.dump(dictionnaire, file)
 
     def openClick(self):
         print("click")
